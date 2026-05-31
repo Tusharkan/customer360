db10fa9063e5733fdba5b6e588e3b15dbd0d5156
Tushar Kant <kanttushar16@gmail.com>
2026-05-31 18:35:14 +0000

    sources setup, dbt-project yml file

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,2 @@
-select
-    1001 as order_id,
-    1 as customer_id,
-    101 as product_id,
-    2 as quantity
+select * from
+{{ source('raw', 'orders')}}
--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,5 +1,3 @@
 
 select
-    101 as product_id,
-    'Laptop' as product_name,
-    1200 as price
+   * from {{ source('raw', 'products')}}
